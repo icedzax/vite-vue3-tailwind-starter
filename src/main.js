959b65f5c052950d92d3282080b209ca965ef1ad
icedzax <your_email_address@icedza@gmail.com>
2022-02-10 09:53:38 +0700

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import { createApp } from 'vue'
-import './tailwind.css'
-import App from './App.vue'
-import { routes } from './routes.js'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createApp } from "vue";
+import { createMetaManager } from "vue-meta";
+import "./tailwind.css";
+import App from "./App.vue";
+import { routes } from "./routes.js";
+import { createRouter, createWebHistory } from "vue-router";
 
-const app = createApp(App)
+const app = createApp(App);
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.use(createMetaManager());
+await router.isReady();
+app.mount("#app");
